7f27be45fb606069eb62263cf9f8bedacd03187a
Wei-Rong <31882359+Wei-Rong@users.noreply.github.com>
2019-10-16 12:20:44 +0800

    tidy up code

--- src/components/authentication/VerifyEmail.js
+++ src/components/authentication/VerifyEmail.js
@@ -1,18 +1,6 @@
-import React, { Component, useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from 'react';
+import {Button,CssBaseline,TextField,Grid,Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Card } from '@material-ui/core';
-import OTPInput, { ResendOTP } from "otp-input-react";
 
 const useStyles = makeStyles(theme => ({
     
